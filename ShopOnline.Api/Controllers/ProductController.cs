5434d9ef05978a79c9bc206da7b4343a98ba8ba8
hazel bozcu <hazelkozlu43@gmail.com>
2022-08-03 22:58:51 +0300

    Add project files.

---
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProducRepository producRepository;
+
+        public ProductController(IProducRepository producRepository)
+        {
+            this.producRepository = producRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }   
+    }
+}
---
+++ ShopOnline.Api/Entities/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopOnline.Api.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId  { get; set; }
+        // the cart entity has a one to many relationship with the cart item entity
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
